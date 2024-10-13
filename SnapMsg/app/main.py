91be3d0466d3c3aca889fe59169a64b14b23d266
brandonschiffer1 <brandonschiffer11@gmail.com>
2024-10-12 21:11:03 -0300

    agrego monitoreo de Datadog

--- SnapMsg/app/main.py
+++ SnapMsg/app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI, HTTPException, Request
+from ddtrace import patch_all
+from ddtrace.contrib.asgi import TraceMiddleware
 from .middleware import ErrorHandlingMiddleware
 from .config import logger
 from .controllers import snap_router
 
+
+patch_all()
+
 app = FastAPI()
+
+app.add_middleware(TraceMiddleware, service="post-microservice", distributed_tracing=True)
+
 app.add_middleware(ErrorHandlingMiddleware)
 
 @app.exception_handler(HTTPException)
@@ -17,12 +25,6 @@ async def custom_http_exception_handler(request: Request, exc: HTTPException):
 app.include_router(snap_router, prefix="/snaps")
 
 logger.info("FastAPI application is starting...")
-
-
-
-
-
-
 
 
 
